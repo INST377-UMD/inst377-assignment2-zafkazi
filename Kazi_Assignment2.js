31fc085359173cab6c3ba7e36872b665189cd157
Zafir Kazi <zkazi02@terpmail.umd.edu>
2025-04-24 12:11:00 -0400

    quote

--- Kazi_Assignment2.js
+++ Kazi_Assignment2.js
@@ -1,3 +1,14 @@
-function Something(){
+//Quote of the day
+if(document.getElementById("quote")){
+    fetch("https://zenquotes.io/api/random")
+        .then(res => res.json())
+        .then(data =>{
+            document.getElementById("quote").textContent = '"' + data[0].q + '"' + " - " + data[0].a;
+
+        })
+}
+
+
+function StartVoice(){
     
 }
